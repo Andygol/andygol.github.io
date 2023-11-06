716b8c3f79153730df33a35707d74f28ff496777
Andygol <Andygol@users.noreply.github.com>
2023-11-06 13:33:37 +0000

    deploy: 9ce7634f5013c82918c2182e5c3357a4ab26d8a1

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 /*!
-* Start Bootstrap - Personal v1.0.1 (https://startbootstrap.com/template-overviews/personal)
+* Start Bootstrap - Andrii Holovin personal website v1.0.3 (https://andygol.co.ua/)
 * Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-personal/blob/master/LICENSE)
+* Licensed under MIT (https://github.com/StartBootstrap/andrii-holovin-personal-website/blob/master/LICENSE)
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
